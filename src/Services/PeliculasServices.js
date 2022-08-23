16a0bc4f2546eb5c28bffc0f711bda32f0aadc9b
cecigiunta <ceciliagiunta57@gmail.com>
2022-08-23 14:44:07 -0300

    prueba

--- src/Services/PeliculasServices.js
+++ src/Services/PeliculasServices.js
@@ -1,11 +1,11 @@
 const apiKey = "api_key=da3403a967b3b32f12431cd7744e3d2a"
 
 export function getAll(){
-    return fetch(`http://api.themoviedb.org/3/discover/movie?${apiKey}&sort_by=popularity.desc`)
+    return fetch(`https://api.themoviedb.org/3/discover/movie?${apiKey}&sort_by=popularity.desc`)
     .then(res => res.json())  
 }
 
 export function getById(id){
-    return fetch(`http://api.themoviedb.org/3/movie"+id+"??${apiKey}`)
+    return fetch(`https://api.themoviedb.org/3/movie/"+id+"??${apiKey}`)
     .then(res => res.json())
 }
